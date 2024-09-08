d3f2f6a0fcb76906dfdb1f773b22a05e3034bea1
4udiwe <arsenijsarifkulov2633@gmail.com>
2024-09-08 18:37:28 +0300

    Добавление Koin

--- app/src/main/java/com/fitlog/app/App.kt
+++ app/src/main/java/com/fitlog/app/App.kt
@@ -1,8 +1,22 @@
 package com.fitlog.app
 
 import android.app.Application
-import com.fitlog.data.db.TrainingProgramDataBase
+import com.fitlog.app.di.appModule
+import com.fitlog.app.di.dataModule
+import com.fitlog.app.di.domainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class App : Application() {
-    val db by lazy { TrainingProgramDataBase.createDB(this) }
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin{
+            androidLogger(Level.DEBUG)
+            androidContext(this@App)
+            modules(listOf(appModule, dataModule, domainModule))
+        }
+    }
 }
